4f7eb5726b5927344c72337223c6750552d89450
Najmul <najmulbinnurul@gmail.com>
2024-04-17 09:21:06 +0600

    rertored

--- apps/user-app/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@repo/db/client";
-const client = new PrismaClient();
-
-const HomePage = () => {
-  return <div className="font-semibold text-5xl">hello</div>;
-};
-
-export default HomePage;
